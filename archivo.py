dbbf4829bf89592792903206eb9e1333cea76bd8
Y0SDan <yosd4n@gmail.com>
2023-04-30 01:28:25 -0600

    dibuje a dwight

--- archivo.py
+++ archivo.py
@@ -5,22 +5,15 @@ print("Hola mundo")
 #hola esta es otra prueba
 print("Cruel")
 
-print("            ___")
-print("        ,-""   `-.              ")
-print("      ,'         `.           ")
-print("     /             \\          ")
-print("    /               \\        ")
-print("   /                 \\     ")
-print("  /_   ___ __ _ _ __|_        ")
-print("  \\ `-' / _` | '__| `\\     ")
-print("   `._.`\__,_|_|  |_,'    ")
-print("         |           ")
-print("        / \\         ")
-print("       /   \\        ")
-print("      /     \\       ")
-print("     /       \\      ")
-print("    /         \\     ")
-print("   /           \\    ")
-print("  /             \\   ")
-print(" /               \\  ")
-print("/_________________\\ ")
+# Abre el archivo en modo lectura
+archivo = open("Dwight.txt", "r")
+
+# Lee el contenido del archivo
+contenido = archivo.read()
+
+# Imprime el contenido en consola
+print(contenido)
+
+# Cierra el archivo
+archivo.close()
+
