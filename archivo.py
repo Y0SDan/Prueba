6c814cc3436f26d0d3ca989a0a7367028aa3f84c
Y0SDan <yosd4n@gmail.com>
2023-04-30 02:24:39 -0600

    Agregue un archivo

--- archivo.py
+++ archivo.py
@@ -1,6 +1,13 @@
 print("esta es mi prueba")
 print("Hola mundo")
-
-
-hola esta es otra prueba
+print("que tal richi")
 print("Cruel")
+
+while(1):
+	print("este es un bucle infinito")
+	
+if(x == 2):
+	print("que tal??")
+elif(x=1):
+	print("esto es lo mas similar a un switch case")
+	
